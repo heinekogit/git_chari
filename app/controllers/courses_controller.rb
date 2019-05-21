4142b53ea59b899ec4aa1408057357b4578aee1f
EC2 Default User <ec2-user@ip-172-31-42-234.ap-northeast-1.compute.internal>
2019-05-21 08:11:48 +0000

    for up

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -18,6 +18,20 @@ class CoursesController < ApplicationController
         Course.create(course_params)
         redirect_to :root
     end
+    
+    
+    def edit
+        @course = Course.find(params[:id])
+        
+    end
+    
+    def update
+        course = Course.find(params[:id])
+        course.update(course_params)
+        redirect_to action: :show
+    end
+    
+    
 
     def copyrecord
         org = Course.find(params[:id])
